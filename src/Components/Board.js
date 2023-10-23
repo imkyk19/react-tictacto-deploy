bdc6d31d92b8c38951c0587234d3bdcf49f1068c
김유경 <gim-yugyeong@kyks-MacBook-Pro.local>
2023-10-23 14:24:13 +1300

    first commit

---
+++ src/Components/Board.js
@@ -0,0 +1,42 @@
+/* Class childclass extends parentsclass
+Component : React library
+I can use Component's function at Square */
+
+import React, {useState } from 'react'
+import Square from './Square'
+import "./Board.css"
+
+const Board = ({squares, onClick}) => {
+
+    const renderSquare = (i) =>{     
+       return <Square value={squares[i]}
+            onClick={()=> onClick(i)}
+        />
+    }
+ 
+    return (
+      <div>
+        
+        <div className='board-row'>
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+        </div>
+        <div className='board-row'>
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+        </div>
+        <div className='board-row'>
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+
+
+  // 다른 컴포넌트에서 사용 할 수 있도록
+  export default Board
+
